100eb53f6208cb3411667d9b1ce9230de0013e0e
yurii-k49 <business.partner.c3@gmail.com>
2025-01-06 00:17:00 +0100

    Update project structure and configurations

--- .eslintrc.js
+++
@@ -1,1 +0,0 @@
-extends: ["some-other-config", "prettier"],
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,46 +1,44 @@
-const { ethers } = require('hardhat');
-
 const networkConfig = {
-  default: {
-    name: 'hardhat',
-    keepersUpdateInterval: '30',
-  },
-  31337: {
-    name: 'localhost',
-    subscriptionId: '588',
-    gasLane:
-      '0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c', // 30 gwei
-    keepersUpdateInterval: '30',
-    raffleEntranceFee: ethers.utils.parseEther('0.01'), // 0.01 ETH
-    callbackGasLimit: '500000', // 500,000 gas
-  },
-  11155111: {
-    name: 'sepolia',
-    subscriptionId: '6926',
-    gasLane:
-      '0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c', // 30 gwei
-    keepersUpdateInterval: '30',
-    raffleEntranceFee: ethers.utils.parseEther('0.01'), // 0.01 ETH
-    callbackGasLimit: '500000', // 500,000 gas
-    vrfCoordinatorV2: '0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625',
-  },
-  1: {
-    name: 'mainnet',
-    keepersUpdateInterval: '30',
-  },
-};
+    default: {
+        name: "hardhat",
+        keepersUpdateInterval: "30",
+    },
+    31337: {
+        name: "localhost",
+        subscriptionId: "588",
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        keepersUpdateInterval: "30",
+        raffleEntranceFee: "100000000000000000", // 0.1 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+    },
+    11155111: {
+        name: "sepolia",
+        subscriptionId: "588",
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
+        keepersUpdateInterval: "30",
+        raffleEntranceFee: "100000000000000000", // 0.1 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+    },
+    1: {
+        name: "mainnet",
+        keepersUpdateInterval: "30",
+    },
+}
 
-const developmentChains = ['hardhat', 'localhost'];
-const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
-const frontEndContractsFile =
-  '../nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json';
-const frontEndAbiFile =
-  '../nextjs-smartcontract-lottery-fcc/constants/abi.json';
+const developmentChains = ["hardhat", "localhost"]
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6
+const frontEndContractsFile = "../nextjs-nft-marketplace-moralis-fcc/constants/networkMapping.json"
+const frontEndContractsFile2 =
+    "../nextjs-nft-marketplace-thegraph-fcc/constants/networkMapping.json"
+const frontEndAbiLocation = "../nextjs-nft-marketplace-moralis-fcc/constants/"
+const frontEndAbiLocation2 = "../nextjs-nft-marketplace-thegraph-fcc/constants/"
 
 module.exports = {
-  networkConfig,
-  developmentChains,
-  VERIFICATION_BLOCK_CONFIRMATIONS,
-  frontEndContractsFile,
-  frontEndAbiFile,
-};
+    networkConfig,
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+    frontEndContractsFile,
+    frontEndContractsFile2,
+    frontEndAbiLocation,
+    frontEndAbiLocation2,
+}
